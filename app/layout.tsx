d785cfe13f9cad706dae3166562a1cfa1db28dff
Inxora <inxora.global@gmail.com>
2026-03-05 15:29:02 +0100

    agregar link meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 // Google Tag Manager ID
 const GTM_ID = 'GTM-WGHBCDFQ'
+// Meta (Facebook) Pixel ID
+const META_PIXEL_ID = '1356325709448784'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://tienda.inxora.com'),
@@ -51,6 +53,29 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        {/* Meta Pixel - se inyecta en head con beforeInteractive */}
+        <Script id="meta-pixel" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');`,
+        }} />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
           />
         </noscript>
 
